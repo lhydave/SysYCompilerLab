72728129a854c56bbc55bfb1bc60e2e65da62a3e
lhydave <lhydave@163.com>
2021-05-11 12:47:17 +0800

    fix some bugs.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
+#include "SysY.tab.hpp"
 #include "eeyore_AST.hpp"
 #include "node.hpp"
 #include "symtab.hpp"
 #include "tigger_gen.hpp"
-#include "SysY.tab.hpp"
 #include <cstdio>
 #include <sstream>
 #include <unistd.h>
@@ -18,27 +18,26 @@ int main(int argc, char **argv)
 {
 	const char *optstring = "Se:o:d";
 	char c;
-	bool gen_eeyore = false;
-	FILE *gen_out = yyin = nullptr;
-	string out_name;
-	bool debug = false;
+	bool gen_asm = false;
+	yyin = nullptr;
+	string eeyoreout_s, tiggerout_s;
 	while ((c = getopt(argc, argv, optstring)) != EOF)
 	{
 		switch (c)
 		{
-		case 'S': gen_eeyore = true; break;
+		case 'S': gen_asm = true; break;
 		case 'e': yyin = fopen(optarg, "r"); break;
-		case 'o': out_name = optarg; break;
-		case 'd': debug = true; break;
+		case 'o': eeyoreout_s = optarg; break;
+		case 't': tiggerout_s = optarg; break;
 		default: printf("Not support yet!\n"); return 0;
 		}
 	}
-	if (!gen_eeyore)
+	if (!gen_asm)
 	{
 		printf("Not support yet!\n");
 		return 0;
 	}
-	if (out_name.empty() || yyin == nullptr)
+	if ((eeyoreout_s.empty() && tiggerout_s.empty()) || yyin == nullptr)
 	{
 		fprintf(stderr, "No file opened.\n");
 		return 1;
@@ -52,16 +51,18 @@ int main(int argc, char **argv)
 	sysY_AST::check_main();
 	if (!has_err) // good!
 	{
+		if (!eeyoreout_s.empty())
+		{
+			auto out = fopen(eeyoreout_s.c_str(), "w");
+			fprintf(out, "%s", sysY_AST::root->code.c_str());
+		}
 		eeyore_AST::build_AST(sysY_AST::root->code);
 		no_alloc::gen_code();
-		fflush(stdout);
-		gen_out = fopen(out_name.c_str(), "w");
-		if (debug)
+		if (!tiggerout_s.empty())
 		{
-			auto debug_out = fopen("eeyore.out", "w");
-			fprintf(debug_out, "%s", sysY_AST::root->code.c_str());
+			auto out = fopen(tiggerout_s.c_str(), "w");
+			fprintf(out, "%s", tigger_dst.str().c_str());
 		}
-		fprintf(gen_out, "%s", tigger_dst.str().c_str());
 	}
 	return 0;
 }
